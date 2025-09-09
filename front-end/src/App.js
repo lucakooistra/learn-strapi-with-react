1d08ea921f3de2573dd5a6cb15d020c23a7e5b3a
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2025-09-09 17:58:35 +0200

    setup react app

---
+++ front-end/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import Category from "./pages/Category";
+import ReviewDetails from "./pages/ReviewDetails";
+import SiteHeader from "./components/SiteHeader";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <SiteHeader />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/details/:id" element={<ReviewDetails />} />
+          <Route path="/category/:id" element={<Category />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
